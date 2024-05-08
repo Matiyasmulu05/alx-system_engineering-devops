11a7bc4a884433da4f634a98385a4e5c5323395c
Matiyasmulu05 <matiyasmulu69@gmail.com>
2024-05-08 18:06:22 +0000

    M

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
-def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
 
-    try:
-        response = requests.get(url, headers=headers)
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         data = response.json()
-        
-        if 'error' in data:
-            return 0
-        
         subscribers = data['data']['subscribers']
         return subscribers
-    except:
+    else:
         return 0
